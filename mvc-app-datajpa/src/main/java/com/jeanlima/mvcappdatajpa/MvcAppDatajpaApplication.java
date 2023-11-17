2490226e241eab409a12958df96f7d2d5e2e0c90
jeanmmlima <jeanmariomlima@gmail.com>
2023-11-17 13:55:12 -0300

    data jpa init

---
+++ mvc-app-datajpa/src/main/java/com/jeanlima/mvcappdatajpa/MvcAppDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.jeanlima.mvcappdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcAppDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcAppDatajpaApplication.class, args);
+	}
+
+}
