0d27e0a03695fb5f1a5bd3606e8bab8e364251c3
jeanmmlima <jeanmariomlima@gmail.com>
2023-11-24 01:42:22 -0300

    first rest

--- spring-rest-api/src/main/java/com/jeanlima/springrestapi/SpringRestApiApplication.java
+++ spring-rest-api/src/main/java/com/jeanlima/springrestapi/SpringRestApiApplication.java
@@ -1,20 +1,7 @@
 package com.jeanlima.springrestapi;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.jeanlima.springrestapi.model.Cliente;
-import com.jeanlima.springrestapi.model.Pedido;
-import com.jeanlima.springrestapi.repository.ClienteRepository;
-import com.jeanlima.springrestapi.repository.PedidoRepository;
 
 @SpringBootApplication
 public class SpringRestApiApplication {
