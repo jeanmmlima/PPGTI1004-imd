ad5a1179f0f9c7f04f02b40b97163373db9483ce
jeanmmlima <jeanmariomlima@gmail.com>
2023-11-24 01:25:59 -0300

    rest api init 2

--- spring-rest-api/src/main/java/com/jeanlima/springrestapi/SpringRestApiApplication.java
+++ spring-rest-api/src/main/java/com/jeanlima/springrestapi/SpringRestApiApplication.java
@@ -19,51 +19,6 @@ import com.jeanlima.springrestapi.repository.PedidoRepository;
 @SpringBootApplication
 public class SpringRestApiApplication {
 
-	/* @Bean
-	public CommandLineRunner init(@Autowired ClienteRepository clientes, @Autowired PedidoRepository pedidos){
-		
-		return args -> {
-			 System.out.println("Salvando clientes");
-            clienteRepository.save(new Cliente("João Maria"));
-            clienteRepository.save(new Cliente("Maria da Glória"));
-
-            boolean existe = clienteRepository.existsByNome("José Mário");
-            System.out.println("Existe cliente com o nome José? "+existe);
-
-            List<Cliente> result = clienteRepository.encontrarPorNomeMod("Maria");
-            result.forEach( System.out::println); 
-
-			System.out.println("Salvando clientes");
-            Cliente fulano = new Cliente("Fulano");
-            clientes.save (fulano);
-
-            Pedido p = new Pedido();
-            p.setCliente(fulano);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-             Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
-            System.out.println(cliente);
-            System.out.println(cliente.getPedidos()); 
-			
-            //pedidos.findByCliente(fulano).forEach(System.out::println);
-
-			List<Pedido> pedidosByCliente = pedidos.findByCliente(fulano); 
-
-			fulano.setPedidos(new HashSet<>(pedidosByCliente));
-
-			fulano.getPedidos().forEach(System.out::println);
-
-
-			
-
-
-            
-		};
-	} */
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiApplication.class, args);
 	}
